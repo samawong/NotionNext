9b9279ee10c93e94c505fe77d83e7f273b31c502
samawong <samafed@gmail.com>
2024-09-04 15:08:18 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_5: '如何打造个人IP',
-  HEO_HERO_TITLE_LINK: 'https://ku.miadnrui.com',
+  HEO_HERO_TITLE_LINK: 'https://www.notion.so/IP-76ae65faf2184ad6af0da75f82baef5f?pvs=4',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
